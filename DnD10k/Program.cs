8b49b93d69a76423f26749991d7e98152ed8052b
gregpedis <trafalgar1618@hotmail.com>
2021-03-07 21:46:10 +0200

    Some webhost shit

--- DnD10k/Program.cs
+++ DnD10k/Program.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
@@ -16,9 +18,23 @@ namespace DnD10k
             CreateHostBuilder(args).Build().Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            var webHost = Host.CreateDefaultBuilder(args).ConfigureWebHostDefaults(
+                webBuilder =>
+                {
+                    var isLinux = RuntimeInformation.IsOSPlatform(OSPlatform.Linux);
+                    webBuilder.UseStartup<Startup>();
+
+                    if (isLinux)
+                    {
+                        var socketPath = Environment.GetEnvironmentVariable("SOCKET_PATH");
+                        webBuilder.UseLibuv().UseKestrel(options => options.ListenUnixSocket(socketPath));
+                    }
+                });
+
+            return webHost;
+        }
     }
 }
 
